f7d2617dbd470cfc89db132f6be6faacc63e9677
Bryan Yao <bryao@seas.upenn.edu>
2022-05-22 03:25:59 -0700

    Update urls and turn off debug mode.

--- bioniccheaper/settings.py
+++ bioniccheaper/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-=b-oyhcxvfskf06xhn6=fp1zkqkzi_2u2-bgp1fr%py)w9e0qx"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "bryanyao.com",
